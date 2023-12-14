b803009a3dee83a7da0b6fb315cd2bda4e573423
Alex <alexfedorko22@gmail.com>
2023-12-14 20:57:37 +0100

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+const d = document;
+const $ = selector => d.querySelector(selector);
+
+const header = $('.header');
+
+window.addEventListener('scroll', () => {
+    const action = scrollY > 50 ? 'add' : 'remove';
+    d.body.classList[action]('header-scrolled');
+});
+
+// Burger menu navigation
+
+const btnBurgerMenu = document.getElementById('burger-btn');
+const mobMenu = document.querySelector('.menu');
+
+if (mobMenu) {
+    mobMenu.addEventListener('click', (e) => {
+        if (e.target.classList.contains('menu-link')) {
+            document.body.classList.remove('menu-active');
+        }
+    })
+}
+
+if (btnBurgerMenu) {
+    btnBurgerMenu.addEventListener('click', () => {
+        document.body.classList.toggle('menu-active');
+    })
+}
+
+
+
+
+
+
+
